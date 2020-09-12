dee62293d6897a20c0bd16d3cfdfea63dc2fffe8
steven <1938848188@qq.com>
2020-09-12 11:00:38 +0800

    添加加载中状态

--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,12 @@
 <template>
   <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
-    <g-icon class="icon" v-if="icon" :name="icon" />
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content"><slot /></div>
   </button>
 </template>
 <script>
 export default {
-  //   props: ["icon", "iconPosition"],
   props: {
     icon: {},
     iconPosition: {
@@ -20,6 +20,14 @@ export default {
 };
 </script>
 <style lang="scss">
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -57,6 +65,9 @@ export default {
       margin-right: 0;
       margin-left: 0.1em;
     }
+  }
+  .loading {
+    animation: spin 2s linear infinite;
   }
 }
 </style>

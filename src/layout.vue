c7e7e66a94512ac69486d2abd43b0dedfd5891f2
steven <1938848188@qq.com>
2020-09-14 23:57:33 +0800

    基本功能完成

---
+++ src/layout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layout" :class="layoutClass">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "MgLayout",
+  data() {
+    return {
+      layoutClass: {
+        hasSilder: false,
+      },
+    };
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
+      if (vm.$options.name === "MgSilder") {
+        this.layoutClass.hasSilder = true;
+      }
+    });
+  },
+};
+</script>
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  border: 1px solid red;
+  &.hasSilder {
+    flex-direction: row;
+  }
+}
+</style>

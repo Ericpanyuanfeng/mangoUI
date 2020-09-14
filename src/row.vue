223c482372ae1358d429efd3ee8e316e262e5a81
steven <1938848188@qq.com>
2020-09-14 14:29:26 +0800

    添加 align=left/right/center 属性

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
@@ -8,6 +8,12 @@ export default {
   name: "MgRow",
   props: {
     gutter: { type: [Number, String] },
+    align: {
+      type: String,
+      validator(value) {
+        return ["left", "right", "center"].includes(value);
+      },
+    },
   },
   mounted() {
     this.$children.forEach((vm) => {
@@ -22,12 +28,24 @@ export default {
         marginRight: -gutter / 2 + "px",
       };
     },
+    rowClass() {
+      let { align } = this;
+      return [align && `align-${align}`];
+    },
   },
 };
 </script>
 <style lang="scss" scoped>
 .row {
   display: flex;
-  margin: 0 -10px;
+  &.align-left {
+    justify-content: flex-start;
+  }
+  &.align-right {
+    justify-content: flex-end;
+  }
+  &.align-center {
+    justify-content: center;
+  }
 }
 </style>

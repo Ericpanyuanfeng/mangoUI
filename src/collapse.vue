e7118ee1adcb9b5517c2a5fcc40cf3663b9d8ef6
steven <1938848188@qq.com>
2020-09-18 20:48:40 +0800

    collapse  基本样式和基本功能完成

---
+++ src/collapse.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "MgCollapse",
+};
+</script>
+<style lang="scss" scoped>
+$gray: #ddd;
+.collapse {
+  border: 1px solid $gray;
+  border-radius: 4px;
+}
+</style>

862806c65c00855015405d35634d59c79017c7aa
steven <1938848188@qq.com>
2020-09-19 15:59:56 +0800

    “创建官网

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "芒果 UI",
+  description: "一个好用的 UI 框架",
+  themeConfig: {
+    sidebar: [
+      { title: "入门", children: ["/install/", "/get-started/"] },
+      { title: "组件", children: ["/components/button"] },
+    ],
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# 芒果 UI 官网
+
+芒果 UI 是一个好用的 UI 框架，提供了 button、tabs、toast 等常用组件，适合移动端和 PC 端使用。
